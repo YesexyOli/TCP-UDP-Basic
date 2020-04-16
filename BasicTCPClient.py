c7385c54d23ce7f20524f505f68cacdacfcdcfb0
olivier <olivier.metzinger@epitech.eu>
2020-04-17 03:00:55 +0900

    tcp et udp fix

--- BasicTCPClient.py
+++ BasicTCPClient.py
@@ -5,20 +5,68 @@
 
 
 from socket import *
+import time
+import sys
 
-serverName = 'nsl2.cau.ac.kr'
-serverPort = 0
+serverName = '127.0.0.1'
+serverPort = 7722
+gettime = lambda: time.time() * 1000
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-print("The client is running on port", clientSocket.getsockname()[1])
 
-message = input('Input lowercase sentence: ')
+#MENU TEXT
+def init_text():
+    print("<Menu>")
+    print("1) convert text to UPPER-case")
+    print("2) get my IP address and port number")
+    print("3) get server time")
+    print("4) get server running time")
+    print("5) exit")
 
-clientSocket.send(message.encode())
+def same_sending():
+    start = gettime()
+    modifiedMessage = clientSocket.recv(2048)
+    end = gettime()
+    final_res = end - start
+    final_res = round(final_res, 1)
+    print('\nReply from server:', modifiedMessage.decode())
+    print('Response time: ', final_res, ' ms\n')
+    start_socket()
 
-modifiedMessage = clientSocket.recv(2048)
-print('Reply from server:', modifiedMessage.decode())
+#FONCTION FOR GET THE SERVER MESSAGE + TIME IT TAKES FOR SERVER/TASK
 
-clientSocket.close()
+def start_socket():
+    init_text()
+    choice_number = input('Input option: ')
+    #GET THE USER NUMBER
+    clientSocket.send(choice_number.encode())
+    while True:
+        if choice_number == "1":
+            sentence_caps = input('Input sentence: ')
+            start = gettime()
+            clientSocket.send(sentence_caps.encode())
+            modifiedMessage = clientSocket.recv(2048)
+            end = gettime()
+            final_res = end - start
+            final_res = round(final_res, 1)
+            print('\nReply from server:', modifiedMessage.decode())
+            print('Response time: ', final_res, ' ms\n')
+            start_socket()
+        elif choice_number == "2" or choice_number == "3" or choice_number == "4":
+            same_sending()
+        elif choice_number == "5":
+            print("\nBye bye~")
+            sys.exit()
+        else:
+            start_socket()
+
+if __name__ == '__main__':
+    try:
+        print("The client is running on port", clientSocket.getsockname()[1])
+        start_socket()
+    #IF CTRL + C
+    except KeyboardInterrupt:
+        print("\nBye bye~")
+        sys.exit()
